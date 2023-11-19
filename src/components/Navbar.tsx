67fd1b38d0b934e8bbe04bb59053fe3ba73792d3
arul2101 <myanuarullah20@gmail.com>
2023-11-20 05:30:13 +0700

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-primary">
+      <h2>MyAnimeList</h2>
+
+      <form action="">
+        <input
+          type="text"
+          name="search"
+          id="search"
+        />
+      </form>
+    </nav>
+  )
+}
